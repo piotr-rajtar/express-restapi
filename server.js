a635f01c634693a7c781572d5e41028a1a2527c0
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-08-27 16:50:57 +0200

    Set server.js file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const path = require('path');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -16,10 +17,17 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
-    console.log('Server is working on port 8000');
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
 });
