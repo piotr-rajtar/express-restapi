38a239ba98cfd23f853626e188e2ba4395953072
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-09-06 20:33:42 +0200

    Add moongose and new database connection

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('./db');
 const path = require('path');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -33,6 +33,15 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 });
+
+// connects our backend code with the database
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', err => console.log('Error ' + err));
 
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
